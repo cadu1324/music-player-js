82808218b13272722042b8d6cd5b9d9c5eb0798b
Carlos Eduardo <cadu11324@gmail.com>
2021-08-02 17:24:11 -0300

    Second commit

--- js/home.js
+++ js/home.js
@@ -106,7 +106,7 @@ const recommendedMusic = () => {
           document.getElementById('spanMobileBarName').innerText = takeName[a.id]
           document.getElementById('spanArtistMusicBar').innerText = takeArtist[a.id]
           document.getElementById('spanMobileBarArtist').innerText = takeArtist[a.id]
-          
+   
         })
       })
       document.querySelectorAll('#svgContentOne').forEach((e) => {
@@ -401,61 +401,41 @@ const playAndPause = () => {
   
   let audio = document.getElementById('audio') 
   
-  
-  if(audio.src.substring(audio.src.length - 3) == 'mp3'){
-    //inputRange
-    document.getElementById('inputMusic').max = parseInt(audio.duration) 
-  
-    //audio
-    if(playOrPause == false){ 
-      audio.play()
-      document.getElementById('pauseButton').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>' 
-    playOrPause = true
-    }else{
-      audio.pause()
-      document.getElementById('pauseButton').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>'
-      playOrPause = false
-    }
-    let p = 1
-    //currentTime
-    const timer = () => {
-      if(s == 60){
-        s = 0
-        m++
-        if(m == 60){
-          m = 0
-          s++
-        }
-      }
-      if(s < 10){
-        s = '0' + s
-      }
-      
-      document.getElementById('currentDuration').innerText = m + ':' + s
-      s++
-      
-      document.getElementById('inputMusic').value = currentInputPercentage
-      currentInputPercentage++
-    }
-    if(startTimer == false){
-     a = setInterval(timer,1000)
-     startTimer = true
-    }else{
-      clearInterval(a)
-      startTimer = false
-    }
-    //totalTime
-    document.getElementById('totalDuration').innerText = Math.floor((parseInt(audio.duration) / 60)) + ':' + Math.floor((audio.duration) % 60)
+  //play/pause
+  if(playOrPause == false){ 
+    audio.play()
+    document.getElementById('pauseButton').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>' 
+  playOrPause = true
+  }else{
+    audio.pause()
+    document.getElementById('pauseButton').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>'
+    playOrPause = false
   }
+
+  setInterval(() => {
+    
+    let currentPosition = audio.currentTime * 100 / audio.duration
+    document.getElementById('inputMusic').value = currentPosition
+
+    //volume
+    audio.volume = document.getElementById('inputVolume').value
+
+    document.getElementById('currentDuration').innerText = Math.floor((parseInt(document.getElementById('audio').currentTime) / 60)) + ':' + Math.floor((document.getElementById('audio').currentTime) % 60)
+
+  }, 1000);
+  
+  document.getElementById('totalDuration').innerText = Math.floor((parseInt(document.getElementById('audio').duration) / 60)) + ':' + Math.floor((document.getElementById('audio').duration) % 60)
 }
 let muteOrUnmute = false
 const changeVolumeIcon = () => {
+  let audio = document.getElementById('audio') 
   if(muteOrUnmute == false){ 
     document.getElementById('btnVolume').innerHTML = '<svg class="svgVolume" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/></svg>' 
     document.getElementById('inputVolume').value = 0
-  muteOrUnmute = true
+    muteOrUnmute = true
   }else{
     document.getElementById('btnVolume').innerHTML = '<svg class="svgVolume" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  ><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>'
+    document.getElementById('inputVolume').value = 0.5
     muteOrUnmute = false
   }
 }
